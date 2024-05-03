72cbeed9c9f1161e1bd3b787b0c3d42826703f5b
zezomo <zezouniverse7@gmail.com>
2024-05-03 13:48:26 -0700

    shell 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,13 +7,13 @@ from fabric.api import *
 from datetime import datetime
 
 def do_pack():
-""" Fabric script that generates a .tgz archive from the contents of the...
-...web_static folder """
-local("sudo mkdir -p versions")
-date = datetime.now().strftime("%Y%m%d%H%M%S")
-filename = "versions/web_static_{}.tgz".format(date)
-result = local("sudo tar -cvzf {} web_static".format(filename))
-if result.succeeded:
-    return filename
-else:
-    return None
+    """ Fabric script that generates a .tgz archive from the contents of the...
+    ...web_static folder """
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        return None
